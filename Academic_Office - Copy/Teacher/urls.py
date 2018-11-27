31bad3a08e679cf0af15df7a2257ab1068c541a4
MDfarazuddin <farazuddin.m17@iiits.in>
2018-11-27 20:11:11 +0530

    corrupt

--- Academic_Office - Copy/Teacher/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<slug:slug>',views.Teacher_Profile,name="T_profile"),
-    path('students/<slug:slug>',views.Show_students,name="Show_students"),
-    path('C_name/',views.Course_contents,name="Course_contents")
-]
--- Academic_Office/Student/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path ,include
-from . import views
-urlpatterns = [
-	path('profile',views.Student_profile,),
-	path('courses/<slug:slug>',views.Student_courses,name="student_courses"),
-	path('profile/grade-sheet',views.Student_grade_sheet,name="student_grades"),
-    path('<slug:slug>',views.Student_profile),
-]
