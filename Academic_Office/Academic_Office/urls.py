96bdacc320b52a0bce06e163068dd077be88108f
MDfarazuddin <farazuddin.m17@iiits.in>
2018-12-06 16:43:35 +0530

    Akshay

--- Academic_Office/Academic_Office/urls.py
+++ Academic_Office/Academic_Office/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path ,include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
@@ -29,3 +32,6 @@ urlpatterns = [
     path('Teachers/',include('Teacher.urls')),
     path('Students/',include('Student.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Academic_Office/Teacher/urls.py
+++ Academic_Office/Teacher/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path('<slug:slug>',views.Teacher_Profile,name="T_profile"),
     path('students/<slug:slug>',views.Show_students,name="Show_students"),
+    path('C_name/<slug:slug>',views.Course_contents,name="Course_contents"),
+    path('C_name/books/upload/', views.upload_book, name='upload_book'),
+    path('C_name/books-assignments/list/<slug:slug>', views.book_list, name='book_list'),
+    path('books/<int:pk>/', views.delete_book, name='delete_book'),
+    path('students/attendance/<slug:slug>',views.take_attendance,name='attendance')
 ]
