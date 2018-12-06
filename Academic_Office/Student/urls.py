2cb7f461ebafdd2ede82fcbea5a051718feb0dc3
MDfarazuddin <farazuddin.m17@iiits.in>
2018-12-06 16:47:15 +0530

    Shashwath

--- Academic_Office/Student/urls.py
+++ Academic_Office/Student/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path ,include
 from . import views
 urlpatterns = [
-	path('profile',views.Student_profile,),
+	path('profile',views.Student_profile,name="Student_profile"),
 	path('courses/<slug:slug>',views.Student_courses,name="student_courses"),
 	path('profile/grade-sheet',views.Student_grade_sheet,name="student_grades"),
     path('<slug:slug>',views.Student_profile),
+    path('courses/your_course/<slug:slug>',views.Student_course_page,name="student_course_page"),
+    path('courses/your_course/to-do-assignments/<slug:slug>',views.Student_assignment_list,name="student_course_page_assignments"),
 ]
